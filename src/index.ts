62201e747d8b0baf29ecbdcc8b87cb94b12989d8
Pedro Felipe <pfsilva190406@gmail.com>
2025-10-01 23:38:17 -0300

    movendo o index de lugar

--- src/index.ts
+++ src/index.ts
@@ -10,6 +10,6 @@ app.listen({ port: 3000, host: '0.0.0.0'}, (err, address) => {
   if (err) {
     console.error(err)
     process.exit(1)
-  }
+  } 
   console.log(`server iniciado porta 3000 em ${address}`)
 })
