361bcc1ac5aa26a52ef91a823ad09d7a7e8d0005
Pedro Felipe <pfsilva190406@gmail.com>
2025-10-07 15:39:40 -0300

    adiciona rota para atualização de notas e ajusta importações de tipos

--- src/modules/notes/notes.routes.ts
+++ src/modules/notes/notes.routes.ts
@@ -1,12 +1,15 @@
-import type { FastifyInstance } from 'fastify'
+import type { FastifyInstance, FastifyRequest } from 'fastify'
 import { type ZodTypeProvider } from 'fastify-type-provider-zod'
 import { 
   type createNoteDTO, 
   createNoteSchema, 
   readNote, 
-  type readNoteDTO
+  type readNoteDTO,
+  type updateNoteDTO,
+  updateNoteSchema
 } from './dto/notes.schema.ts'
 import { NoteController } from './note.controller.ts'
+import { request } from 'http'
 
 const controller = new NoteController()
 
@@ -40,6 +43,26 @@ export async function notesRoutes(server: FastifyInstance) {
     async (request, reply) => {
       try {
         const note = await controller.read(request.body)
+        reply.status(200).send(note)
+      } catch (error) {
+        reply.status(500).send(error)
+      }
+    }
+  )
+
+  app.post<{ Params: { id: any }; Body: updateNoteDTO }>(
+    '/notes/update/:id',
+    {
+      schema: {
+        body: updateNoteSchema
+      }
+    },
+    async (request, reply) => {
+      const { id } = request.params
+      const update = request.body
+
+      try {
+        const note = await controller.update(id, update)
         reply.status(200).send(note)
       } catch (error) {
         reply.status(500).send(error)
